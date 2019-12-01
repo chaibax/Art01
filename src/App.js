779f7b85fd9006b64f8fabd1773da0b1c3211eee
Art01 <art01@macbook-pro-de-chaib-martinez.home>
2019-12-01 21:42:51 +0100

    fiat lux

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import { useAuth0 } from "./react-auth0-wrapper";
+import './App.css';
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import PrivateRoute from "./components/PrivateRoute";
+
+
+function App() {
+
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    );
+  }
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <header className="App-header">
+          <h1>
+            Test auth0 client side with react
+          </h1>
+          <p>
+          <NavBar />
+          </p>
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
