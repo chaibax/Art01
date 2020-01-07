81f8de50d6e731ee25f54b9cfe85f5b89e4aad17
Art01 <art01@macbook-pro-de-chaib-martinez.home>
2020-01-07 12:30:55 +0100

    add home

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from "./NavBar";
+
+const Home = () => {
+  return (
+    <header className="App-header">
+    <h1>
+      Test auth0 client side with react for Art01
+    </h1>
+    
+    <NavBar />
+    
+  </header>
+    )
+}
+
+export default Home
