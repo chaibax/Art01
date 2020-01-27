fe3b7ec39bccf67ef834ccac627bcec4368422ff
Art01 <art01@macbook-pro-de-chaib.home>
2020-01-27 17:54:23 +0100

    Better ip fetching

--- src/components/Paint.js
+++ src/components/Paint.js
@@ -1,52 +1,35 @@
 import React, { Fragment } from "react";
 import { useAuth0 } from "../react-auth0-wrapper";
-import AsyncFetcher from "react-async-fetcher";
-import ViewColorFromIp from "./ViewColorFromIp";
+import ViewColorFromIp from "./ViewColorFromIp"; // a degager
 
 const Paint = () => {
   const { loading, user } = useAuth0();
+  const FirstIP = user['https://art01/FirstIP'];
   if (loading || !user  ) {
     return (
       <div>Loading paint page...</div>
     );
   }
   return (
-      <h2>Hello {user.given_name}, yout IP is 
-      <AsyncFetcher url="https://api.ipify.org?format=json">
-    {({ isLoading, error, data }) => {
-      // some loading state...
-      if (isLoading) {
-        return <p>Searching your ip...</p>;
-      }
-
-      // if has any error in your request
-      if (error) {
-        return (
-          <p>
-            <strong>Error:</strong> {error}
-          </p>
-        );
-      }
-      return (
+      <h2>Hello {user.given_name}, yout IP is ==> {FirstIP} ==
+     
         <Fragment>
         <div>  
         <p>
-          {data.ip}
+          {FirstIP}
          </p>
 
           So, your pixel color is : 
           <br/>
           <br/>            
-           <ViewColorFromIp ip={data.ip} />
+           <ViewColorFromIp ip={FirstIP} />
          
         <p>
           <button className="button">Paint my pixel</button>
         </p>
         </div>
         </Fragment>
-      );
-    }}
-  </AsyncFetcher>
+
       </h2>
      
 
