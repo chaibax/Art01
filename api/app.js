dd2a2f3f904f90364eff458cbebac6867214c0ed
Art01 <art01@macbook-pro-de-chaib.home>
2020-02-11 18:55:48 +0100

    testing evenst storage

--- api/app.js
+++ api/app.js
@@ -8,11 +8,32 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var eventstore = require('eventstore');
-var debug = require('debug');
+var debug = require('debug')('api');
 
 var app = express();
 var es = eventstore();
 
+
+
+
+var es = require('eventstore')({
+  type: 'mongodb', // ca marche avec inMemory
+  eventsCollectionName: 'events',             // optional
+  snapshotsCollectionName: 'snapshots',       // optional
+  transactionsCollectionName: 'transactions', // optional
+  timeout: 10000,
+  url : process.env.MONGODB_URI
+});
+
+es.on('connect', function() {
+  debug('storage connected');  
+  });
+
+  es.on('disconnect', function() {
+    debug('connection to storage is gone');
+  });
+
+const uuidv1 = require('uuid/v1');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,4 +45,27 @@ app.use('/api/', indexRouter);
 app.use('/api/users', usersRouter);
 
 module.exports = app;
-console.log('>>> '+process.env.MONGODB_URI);
+debug('>>> '+process.env.MONGODB_URI+' Host= '+process.env.MONGO_HOST+'PORT= '+process.env.MONGO_PORT);
+debug('toto');
+
+
+
+
+
+
+
+es.getEventStream('streamId', function(err, stream) {
+  stream.addEvent({ my: 'event' });
+  stream.addEvents([{ my: 'event2' }]);
+  stream.commit(function(err, stream) {
+  console.log(stream.eventsToDispatch); // this is an array containing all added events in this commit.
+  });
+  });
+
+  es.init(); // callback is optional
+
+/*
+es.getEventStream(666, function(err, stream) {
+  stream.addEvent({ new: 'nop' });
+  stream.commit();
+});*/
