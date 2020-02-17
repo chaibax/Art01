4203e51432b62fd977654e1392ccca302f0e363b
Art01 <art01@macbook-pro-de-chaib.home>
2020-02-17 12:15:27 +0100

    playing with node-eventstore

--- api/app.js
+++ api/app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var eventstore = require('eventstore');
@@ -14,26 +12,49 @@ var app = express();
 var es = eventstore();
 
 
-
-
 var es = require('eventstore')({
   type: 'mongodb', // ca marche avec inMemory
-  eventsCollectionName: 'events',             // optional
-  snapshotsCollectionName: 'snapshots',       // optional
-  transactionsCollectionName: 'transactions', // optional
-  timeout: 10000,
-  url : process.env.MONGODB_URI
+  url: process.env.MONGODB_URI,                        // optional
+  eventsCollectionName: 'events',                // optional
+  snapshotsCollectionName: 'snapshots',          // optional
+  transactionsCollectionName: 'transactions',    // optional
+  positionsCollectionName: 'positions'
+
 });
 
-es.on('connect', function() {
-  debug('storage connected');  
+es.init(function (err) {
+  es.defineEventMappings({
+    id: 'id',
+    commitId: 'commitId',
+    commitSequence: 'commitSequence',
+    commitStamp: 'commitStamp',
+    streamRevision: 'streamRevision'
   });
 
-  es.on('disconnect', function() {
+
+  es.on('connect', function () {
+    debug('storage connected');
+  });
+
+  es.on('disconnect', function () {
     debug('connection to storage is gone');
   });
 
-const uuidv1 = require('uuid/v1');
+
+  es.getEventStream('pixels', function (err, stream) {
+    stream.addEvent({ my: 'event' });
+    stream.addEvents([{ my: 'event2' }]);
+    stream.commit(function (err, stream) {
+      console.log(stream.eventsToDispatch); // this is an array containing all added events in this commit.
+    });
+
+
+  });
+
+});
+
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -45,27 +66,5 @@ app.use('/api/', indexRouter);
 app.use('/api/users', usersRouter);
 
 module.exports = app;
-debug('>>> '+process.env.MONGODB_URI+' Host= '+process.env.MONGO_HOST+'PORT= '+process.env.MONGO_PORT);
-debug('toto');
 
 
-
-
-
-
-
-es.getEventStream('streamId', function(err, stream) {
-  stream.addEvent({ my: 'event' });
-  stream.addEvents([{ my: 'event2' }]);
-  stream.commit(function(err, stream) {
-  console.log(stream.eventsToDispatch); // this is an array containing all added events in this commit.
-  });
-  });
-
-  es.init(); // callback is optional
-
-/*
-es.getEventStream(666, function(err, stream) {
-  stream.addEvent({ new: 'nop' });
-  stream.commit();
-});*/
