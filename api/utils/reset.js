f620ba9cd51c727ceb36b1af3e8f4a9cd374e960
Art01 <art01@MacBook-Pro-de-Chaib.local>
2020-04-20 16:10:06 +0200

    debug mode

---
+++ api/utils/reset.js
@@ -0,0 +1,87 @@
+require('dotenv').config();
+const fs = require('fs');
+var glob = require("glob");
+
+//database cleaning
+const mongoose = require('mongoose'), Schema = mongoose.Schema;
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });   
+
+const connection = mongoose.connection;
+connection.once("open", function() {
+console.log("MongoDB connected using Mongoose.");
+
+connection.db.dropCollection(
+    'events',
+function(err, result) {
+console.log("Collection events droped");
+}
+);
+
+connection.db.dropCollection(
+    'positions',
+function(err, result) {
+console.log("Collection positions droped");
+}
+);
+
+
+//file cleaning 
+
+const path = '../public/images/Art0x.png';
+const emptypath = '../public/images/empty.png';
+fs.unlink(path, (err) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+  
+    console.log(path+' removed');
+  })
+
+// destination.txt will be created or overwritten by default.
+fs.copyFile(emptypath,path, (err) => {
+    if (err) throw err;
+    console.log(emptypath+' was copied to '+path);
+  });
+
+  glob('../public/images/art*.png', function(err, files) {
+    
+var arrayLength = files.length;
+
+for (var i = 0; i < arrayLength; i++) {
+   fs.unlink(files[i], (err) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log(files[i]+' removed !');
+  })
+}
+/*
+    fs.unlink(element.toString(), (err) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        console.log(files.toString()+' removed !');
+      })
+*/
+
+
+
+});
+
+/*
+fs.unlink(path, (err) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+
+  //file removed
+})
+
+*/
+
+
+});
