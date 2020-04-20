e83b8bda3bb7d043ea1b82ab6945e758fdc2f1fd
Art01 <art01@MacBook-Pro-de-Chaib.local>
2020-04-20 22:08:21 +0200

    logout fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,8 @@ import { useAuth0 } from "../react-auth0-wrapper";
 const NavBar = () => {
   const { isAuthenticated, logout } = useAuth0();
 
+
+
   return (
     <nav className="navbar is-fixed-bottom shadowed blackbackground" role="navigation" aria-label="main navigation" style={{zIndex: 0}}>
 
@@ -12,7 +14,7 @@ const NavBar = () => {
         isAuthenticated && (
           <div className="navbar-menu is-active shadowed blackbackground"  >
             <div className="navbar-end"> 
-                <a className="navbar-item has-text-centered" onClick={() => logout()}>
+                <a className="navbar-item has-text-centered" onClick={() => logout({returnTo: window.location.origin})}>
                 > logout &nbsp;
                 </a>
 
