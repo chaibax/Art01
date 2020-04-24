73f085a938a66ce573d070a01981c489e91ca763
Art01 <art01@MacBook-Pro-de-Chaib.local>
2020-04-24 17:38:25 +0200

    minor change

--- src/components/View.js
+++ src/components/View.js
@@ -2,19 +2,22 @@ import React, { Component, Fragment } from "react";
 import { useAuth0 } from "../react-auth0-wrapper";
 import '../images.css';
 
+
+
 class View  extends Component  {
 
     constructor(props){
         super(props);
 
+        
         this.state = {
             link: process.env.REACT_APP_API_BASE_URL + "/../images/Art0x.png",
             position : parseInt(props.match.params.id),
-            pixeladded_image : process.env.REACT_APP_API_BASE_URL + "/../images/art" + parseInt(props.match.params.id) + ".png",
+            pixeladded_image : process.env.REACT_APP_API_BASE_URL + "/../images/art" + parseInt(props.match.params.id) + ".png", 
             compteur : 0
           }
-       
       }
+
 
       refresh(){
         console.log('refresh!');
@@ -24,7 +27,7 @@ class View  extends Component  {
       }
       componentDidMount(){
        console.log('mounted');
-       this.interval = setInterval(() => this.refresh(), 1000);
+       this.interval = setInterval(() => this.refresh(), 10000);
       }
 
       componentWillUnmount() {
@@ -32,7 +35,9 @@ class View  extends Component  {
       }
 
     render() {
+       
         return(
+            
         <Fragment>
             <h1 className="title is-size-2 has-text-centered shadowed">
          ART01 now. Your are the painter #{this.state.position + 1}
@@ -41,10 +46,8 @@ class View  extends Component  {
             <div className="imageblink ">
                 <div className="has-text-centered">
 
-
-                    <a href={this.state.link} download="Art01.png" target="_blank">
-                        <img src={this.state.pixeladded_image} className="bottom piximagerendering" style={{ backgroundColor: 'rgba(255, 255, 255, 1)', height: '80vmin', width: '80vim', zIndex: 1 }} />
-                    </a>
+               
+                  <a href={this.state.link} download="Art01.png" target="_blank"><img src={this.state.pixeladded_image} className="bottom piximagerendering" style={{ backgroundColor: 'rgba(255, 255, 255, 1)', height: '80vmin', width: '80vim', zIndex: 1 }} /></a>
                     <a href={this.state.link} download="Art01.png" target="_blank">
                         <img src={this.state.link} className="imagetop piximagerendering" style={{  backgroundColor: 'rgba(255, 255, 255, 1)', height: '80vmin', width: '80vim', zIndex: 1 }} />
                     </a>
